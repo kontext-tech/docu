0750f4e00ec0cbae88c798156a6c034a0d5e6d86
Raymond Tang <fahao.tang@hotmail.com>
2018-07-01 00:27:58 +1000

    Ported project Utils

---
+++ src/Kontext.Utils/Sitemap/SitemapProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Kontext.Utils.Sitemap
+{
+    /// <summary>
+    /// Sitemap provider
+    /// </summary>
+    public class SitemapProvider
+    {
+        ICollection<SitemapUrl> urls;
+
+        public SitemapProvider()
+        {
+            Urls = new Collection<SitemapUrl>();
+        }
+
+        public ICollection<SitemapUrl> Urls { get => urls; set => urls = value; }
+
+        /// <summary>
+        /// Add url
+        /// </summary>
+        /// <param name="url"></param>
+        public void AddUrl(SitemapUrl url) => this.Urls.Add(url);
+
+        public void AddUrl(string url, double? priority = null, DateTime? lastModified = null, ChangeFrequency changeFrequency = ChangeFrequency.Weekly)
+        {
+            this.Urls.Add(new SitemapUrl { Url = url, ChangeFrequency = changeFrequency, Priority = priority, LastModified = lastModified });
+        }
+
+        public void AddUrl(string url, DateTime lastModified)
+        {
+            this.Urls.Add(new SitemapUrl { Url = url, LastModified = lastModified });
+        }
+    }
+}
---
+++ src/Kontext.Utils/Sitemap/SitemapUrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kontext.Utils.Sitemap
+{
+    public class SitemapUrl
+    {
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public DateTime? LastModified { get; set; }
+        public double? Priority { get; set; }
+        public string Url { get; set; }
+    }
+}
