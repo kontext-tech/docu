0c62d3192506fed8a4dc32110c8be97a701ed1b8
Raymond Tang <fahao.tang@hotmail.com>
2018-07-01 00:09:01 +1000

    Ported project Security; renamed project names

---
+++ src/Kontext.Security/AuthorizationPolicies/AssignRolesRequirement.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kontext.Security.AuthorizationPolicies
+{
+    public sealed class AssignRolesRequirement : IAuthorizationRequirement
+    {
+
+
+    }
+
+    public class AssignRolesHandler : AuthorizationHandler<AssignRolesRequirement, Tuple<string[], string[]>>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AssignRolesRequirement requirement, Tuple<string[], string[]> newAndCurrentRoles)
+        {
+            if (context.User.HasClaim(ApplicationClaimTypes.Permission, DefaultApplicationPermissionProvider.AssignRoles) || !GetIsRolesChanged(newAndCurrentRoles.Item1, newAndCurrentRoles.Item2))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+
+        private bool GetIsRolesChanged(string[] newRoles, string[] currentRoles)
+        {
+            if (newRoles == null)
+                newRoles = new string[] { };
+
+            if (currentRoles == null)
+                currentRoles = new string[] { };
+
+            bool roleAdded = newRoles.Except(currentRoles).Any();
+            bool roleRemoved = currentRoles.Except(newRoles).Any();
+            return roleAdded || roleRemoved;
+        }
+    }
+}
